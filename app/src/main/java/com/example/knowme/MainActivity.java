1866edbbc0e90c24d5cd54167313b0e06470a28d
rgus100 <verma.22@iitj.ac.in>
2021-08-02 18:52:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/knowme/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.knowme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button yes;
+    Button no;
+    TextView textView2;
+    private String questions[] = {"Is owner Ravi?", "Is owner a student of IIT Kanpur?", "Is owner a bad person?", "Is Owner going to be selected in your company?",
+            "Do you like to recommend such apps to someone starting android dev?"};
+
+    private boolean answers[] = {true,false,false,true,true};
+    private int index = 0;
+    private int score = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        yes = findViewById(R.id.yes);
+        no = findViewById(R.id.no);
+        textView2 = findViewById(R.id.textView2);
+        textView2.setText(questions[index]);
+
+        yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (index<questions.length){
+                    if (answers[index]==true){
+                        score++;
+                    }
+                    index++;
+                    if (index<questions.length){
+                        textView2.setText(questions[index]);
+                    }
+                    else{
+                        Toast.makeText(MainActivity.this, "Your Score is " + score, Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        });
+        no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (index<questions.length){
+                    if (answers[index]==false){
+                        score++;
+                    }
+                    index++;
+                    if (index<questions.length){
+                        textView2.setText(questions[index]);
+                    }
+                    else{
+                        Toast.makeText(MainActivity.this, "Your Score is " + score, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+    }
+}
